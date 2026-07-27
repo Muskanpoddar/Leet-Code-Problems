f371f56e59a19dbe6c8dcb6b0c2b62a459f6ff67
Muskan Poddar <158065188+Muskanpoddar@users.noreply.github.com>
2026-07-27 18:05:55 +0530

    0011-CONTAINER-WITH-MOST-WATER Stats: Time: 5 ms (82.83%), Space: 77.3 MB (49.44%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,21 +1,24 @@
 public class Solution {
     public int maxArea(int[] height) {
-        int left = 0, right = height.length - 1;
+        int left =0;
+        int right = height.length -1;
         int maxArea = 0;
-        while (left < right) {
-            int h = Math.min(height[left], height[right]);
-            maxArea = Math.max(maxArea, h * (right - left));
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
+
+
+        while(left < right){
+            int width = right - left ;
+            int h = Math.min(height[left] , height[right]);
+            int area = width * h;
+
+            maxArea = Math.max(maxArea , area);
+        
+        if(height[left] <height[right]){
+            left++;
+        }else{
+            right--;
+        }
         }
         return maxArea;
     }
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] height = {1,8,6,2,5,4,8,3,7};
-        System.out.println("Max area: " + solution.maxArea(height));
-    }
+    
 }
